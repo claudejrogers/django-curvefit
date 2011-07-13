82526f63ab272e1ccfa96bde80a7921115f420ee
Claude Rogers <claude.j.rogers@gmail.com>
2011-07-13 10:15:07 -0700

    added file_handler to read xls or text data, and corresponding test

--- curvefit/tests.py
+++ curvefit/tests.py
@@ -4,8 +4,13 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import os
+import xlrd, xlwt
 
 from django.test import TestCase
+
+from settings import MEDIA_ROOT
+from curvefit.file_handler import file_handler
 
 
 class SimpleTest(TestCase):
@@ -14,3 +19,29 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class FileHandlerTest(TestCase):
+    """
+    Test that the file_handler function works and returns expected values.
+    """
+    def test_read_xls_files(self):
+        """
+        Test that xls files are read and data is correct.
+        """
+        filename = os.path.join(MEDIA_ROOT, "test.xls")
+        # Write fake data to xls file.
+        wbk = xlwt.Workbook()
+        sheet = wbk.add_sheet('sheet 1')
+        for i in range(15):
+            sheet.write(i, 0, i)
+            sheet.write(i, 1, i * i)
+        wbk.save(filename)
+        # test
+        x, y, m = file_handler(filename, '.xls')
+        self.assertEqual(x, range(15))
+        self.assertEqual(y, [i*i for i in range(15)])
+        # clean up
+        os.remove(filename)
+    def test_read_csv_files(self):
+        
+        
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 
 import os
 
-PROJECT_PATH = os.path.abspath(os.dirname(__file__))
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -15,12 +15,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(PROJECT_PATH, os.path.join('db', 'mydb.sqlite3')),
     }
 }
 
